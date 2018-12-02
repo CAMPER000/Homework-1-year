c7d358a1b899742a4782c59f76c241406a483409
Nikola Todorov <44548080+CAMPER000@users.noreply.github.com>
2018-12-02 18:22:28 +0200

    Add files via upload

---
+++ Chapter 7/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишете програма, която намира максимална редица от последо-
+            вателни еднакви елементи в масив. Пример: {2, 1, 1, 2, 3, 3, 2, 2,
+            2, 1} → {2, 2, 2}.*/
+            int br = 1, count = 1, number = 0;
+
+            Console.Write("Enter array length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1]) count++;
+                else count = 1;
+
+                if (count > br)
+                {
+                    br = count;
+                    number = arr[i];
+                }
+            }
+
+            for (int i = 0; i < br; i++) Console.Write("{0}, ", number);
+        }
+    }
+}
